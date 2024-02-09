47b95a94739a8302882ea70dcae14e3f9929b6c8
Skyerv <amanda@manzoti.com>
2024-02-08 21:11:52 -0300

    feat: mapeamento e reconhecimento de localizações com coordenadas provisórias

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:highlight_text/highlight_text.dart';
+import 'package:latlng/latlng.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:latlng/latlng.dart' as latLng;
 
 void main() {
   runApp(const MyApp());
@@ -36,17 +38,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late String _destination = "";
+
   final Map<String, HighlightedWord> _highlights = {
-    'ir': HighlightedWord(
-      onTap:() => print("ir"),
-      textStyle: const TextStyle(
-        color: Colors.blue,
-        fontWeight: FontWeight.bold,
-        fontSize: 36,
-      ),
-    ),
     'biblioteca': HighlightedWord(
-      onTap:() => print("biblioteca"),
+      onTap: () => print("142.0.210"),
       textStyle: const TextStyle(
         color: Colors.red,
         fontWeight: FontWeight.bold,
@@ -54,13 +50,26 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     ),
     'bloco L': HighlightedWord(
-      onTap:() => print("bloco L"),
+      onTap: () => print("bloco L"),
       textStyle: const TextStyle(
         color: Colors.blue,
         fontWeight: FontWeight.bold,
         fontSize: 36,
       ),
     ),
+  };
+
+  final Map<String, String> _locations = {
+    'biblioteca':
+        '-25.051865882571917, -50.130233731119866', //LatLng((-25.05179784592007) as Angle, (-50.13033029064471) as Angle),
+    'bloco L':
+        '14.23123', //LatLng((-25.05179784592007) as Angle, (-50.13033029064471) as Angle),
+    'bloco C':
+        '15.33212', //LatLng((-25.05179784592007) as Angle, (-50.13033029064471) as Angle),
+    'ru':
+        '12.424224', //LatLng((-25.05179784592007) as Angle, (-50.13033029064471) as Angle),
+    'restaurante universitário':
+        '11.22222', //LatLng((-25.05179784592007) as Angle, (-50.13033029064471) as Angle),
   };
 
   late stt.SpeechToText _speech;
@@ -97,14 +106,21 @@ class _MyHomePageState extends State<MyHomePage> {
         reverse: true,
         child: Container(
           padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
-          child: TextHighlight(
-            text: _text,
-            words: _highlights,
-            textStyle: const TextStyle(
-              fontSize: 32.0,
-              color: Colors.black,
-              fontWeight: FontWeight.w400,
-            ),
+          // child: TextHighlight(
+          //   text: _text,
+          //   words: _highlights,
+          //   textStyle: const TextStyle(
+          //     fontSize: 32.0,
+          //     color: Colors.black,
+          //     fontWeight: FontWeight.w400,
+          //   ),
+          // ),
+          child: Text(
+            _destination,
+            style: TextStyle(
+                fontSize: 24.0,
+                color: Colors.teal[600],
+                fontWeight: FontWeight.bold),
           ),
         ),
       ),
@@ -121,13 +137,26 @@ class _MyHomePageState extends State<MyHomePage> {
       if (available) {
         setState(() => _isListening = true);
         _speech.listen(
-          onResult: (val) => setState(() {
-            _text = val.recognizedWords;
-            if (val.hasConfidenceRating && val.confidence > 0) {
-              _confidence = val.confidence;
-            }
-          })
-        );
+            onResult: (val) => setState(() {
+                  _text = val.recognizedWords;
+                  if (val.hasConfidenceRating && val.confidence > 0) {
+                    _confidence = val.confidence;
+                  }
+
+                  print(val.recognizedWords.toString().toLowerCase());
+
+                  List<String> words =
+                      val.recognizedWords.toLowerCase().split(' ');
+
+                  for (String word in words) {
+                    if (_locations.containsKey(word)) {
+                      _destination = 'Coordenadas de $word: ${_locations[word]!}';
+                      return;
+                    }
+                  }
+
+                  _destination = 'Destino não reconhecido';
+                }));
       }
     } else {
       setState(() => _isListening = false);
